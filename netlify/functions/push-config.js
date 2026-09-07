34c08af4ffd3f4ba768a494a97e5f275c483a0c1
4v8rdmy9hf-maker <4v8rdmy9hf@privaterelay.appleid.com>
2026-09-07 13:42:19 +0200

    Update push-config.js

--- netlify/functions/push-config.js
+++ netlify/functions/push-config.js
@@ -1,7 +1,64 @@
-const { json } = require("./_firebase");
+const {
+  json
+} = require("./_firebase");
 
-exports.handler = async () => {
-  return json(200, {
-    publicKey: process.env.VAPID_PUBLIC_KEY || ""
-  });
+
+exports.handler = async event => {
+
+  if (event.httpMethod !== "GET") {
+
+    return json(
+      405,
+      {
+        error:
+          "Methode nicht erlaubt"
+      }
+    );
+
+  }
+
+
+  try {
+
+    const publicKey =
+      process.env.VAPID_PUBLIC_KEY ||
+      "";
+
+
+    if (!publicKey) {
+
+      return json(
+        500,
+        {
+          error:
+            "Push-Konfiguration fehlt"
+        }
+      );
+
+    }
+
+
+    return json(
+      200,
+      {
+        publicKey
+      }
+    );
+
+
+  } catch (error) {
+
+    console.error(error);
+
+
+    return json(
+      500,
+      {
+        error:
+          "Push-Konfiguration konnte nicht geladen werden"
+      }
+    );
+
+  }
+
 };
